3a9bcfd851729585a3704e528a1f26291eb4df59
GabouKing <gribeirodelimas2@gmail.com>
2023-10-31 23:13:16 -0300

    Alterando tamanho da fonte

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-4 sm:p-8 md:p-12 lg:p-16 xl:p-24 bg-green-900">
       <p className="font-alt text-4xl sm:text-7xl md:text-6xl lg:text-5xl xl:text-9xl text-white-500">Gabriel e Beatriz</p>
       <div className='flex flex-1 flex-col justify-center align-center m-auto'>
-        <p className='font-sans text-sm md:text-base lg:text-lg px-6 py-8 text-white-100 m-auto w-96'>Essa é a lista de presente do nosso casamento, 
+        <p className='font-sans text-2xl md:text-base lg:text-lg px-6 py-8 text-white-100 m-auto w-96'>Essa é a lista de presente do nosso casamento, 
           porém a sua presença é mais importante para nós!
           Caso deseje nos presentear, segue as opções na lista.</p>
         <a href="/presentes" className='flex justify-center hover:cursor-default'>
